5b08d28f1aeeb3b99affc71b40777f6baf12ab46
代言宝 <ceo@daiyanbao.com>
2015-07-03 14:05:43 +0000

    commit

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
+git add .
 git commit -m "commit"
-git add .                                 #把所有新回的文件加入repo  
-git commit -a  
-  
-git push origin master 
+#git add .                                 #把所有新回的文件加入repo  
+#git commit -a  
+git push origin master -n
